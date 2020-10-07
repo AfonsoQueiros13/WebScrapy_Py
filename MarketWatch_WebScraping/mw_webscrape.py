3a5311d09297f5223bfbd3c03f2ac243cc040dd5
Hadoop <hadoop@masternode-HP-ProBook-650-G1.home>
2020-10-07 16:44:53 +0100

    marketwatch scrape implemented

--- MarketWatch_WebScraping/mw_webscrape.py
+++ MarketWatch_WebScraping/mw_webscrape.py
@@ -1,23 +1,36 @@
-from lxml \
-import html #
-import requests #import librarie
-import xlrd, xlwt 
+from bs4 import BeautifulSoup
+import urllib.request
+import csv
 import sys
 
+stock = sys.argv[1]
 
-print('Stock ticket: ', sys.argv[1:])
-url = ("https://www.marketwatch.com/investing/stock/%s" %sys.argv[1:])
-urlopen = requests.get(url)
-tree = html.fromstring(urlopen.content)
-price = tree.xpath("//div[@class='quotedisplay tenwidequote pre']/table/div[5]/div/p/p/text()")
-print(price)
-description = tree.xpath('//p[@class="description__text"]/text()')
+#atributes of stock
+attributes = ["Price","Open", "Day Range","52 Week","MarketCap",
+"Racio","Shares Outst.","Public Float","Beta",
+"Rev per empl.","PE Ratio","EPS","Yield","Dividend","Ex Divid Rate","Short Interest",
+"Percent of Float Shorted","Avg volume"]
+
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36')]
+response = opener.open('https://www.marketwatch.com/investing/stock/'+stock)
+
+soup = BeautifulSoup(response, 'html.parser')
+#price = soup.find ('h3',{'class':'intraday__price'})
+price = soup.find ('bg-quote',{'class':'value'})
+
+print(price.text)
+summarry = soup.find_all('span',{'class':'primary kv__primary'}) 
+#print (summarry)
+summarry_atb = []
+summarry_atb.append(price.text)
 
 
-low_high = tree.xpath('//span[@class="kv__value kv__primary "]/text()')
-#print('Description: ', description[0])
-print('Current stock price: $',price[0])
-print('Open stock price: $', low_high[0])
-print('Todays high and low prices: Low', low_high[1], 'High')
-
-
+with open(stock+'_marketwatch.csv','w', newline='') as file:
+    writer = csv.writer(file,delimiter="\t")
+    writer.writerow(attributes)
+    #writer.writerow(price)
+    for item in summarry:
+       summarry_atb.append(item.text)
+    writer.writerow(summarry_atb)
+file.close()
